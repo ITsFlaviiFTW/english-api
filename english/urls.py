dc08b02cad7dd63d09164e49528320e65a8a4202
Flavius Porumbiel <Fporumbiel8739@conestogac.on.ca>
2025-08-26 21:29:09 -0400

    had an internal error - 500...trying something

--- english/urls.py
+++ english/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HealthView, RegisterView, LoginView, MeView
+from .views import HealthView, RegisterView, LoginView, MeView, MeSummaryView
+from . import views
 
 urlpatterns = [
     path("health/", HealthView.as_view()),
